0daaa2bbc6c7ee8d8c4a4d6eaabb3acef85c87e0
Rafet <rafetbasturk@yahoo.com>
2023-07-29 13:23:53 +0300

    added global loading

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -7,6 +7,7 @@ export { default as FormRowSelect } from "./FormRowSelect";
 export { default as Job } from "./Job";
 export { default as JobInfo } from "./JobInfo";
 export { default as JobsContainer } from "./JobsContainer";
+export { default as Loading } from "./Loading";
 export { default as Logo } from "./Logo";
 export { default as LogoutContainer } from "./LogoutContainer";
 export { default as Navbar } from "./Navbar";
