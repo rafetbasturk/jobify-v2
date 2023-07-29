d0f600286a9bff8f56475542b57ebe2ca2b273ce
Rafet <rafetbasturk@yahoo.com>
2023-07-29 11:58:24 +0300

    change upload image settings from disk to memory

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middlewares/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -22,8 +22,8 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file)
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
